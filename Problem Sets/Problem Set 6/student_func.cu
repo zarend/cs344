1797b2888321946b2da9f61074d0f5631b01cdd6
zarend <zacharyarend@gmail.com>
2014-12-15 22:05:07 -0800

    solves problem set 6 (114.908447 ms)

--- Problem Sets/Problem Set 6/student_func.cu
+++ Problem Sets/Problem Set 6/student_func.cu
@@ -66,13 +66,325 @@
 
 #include "utils.h"
 #include <thrust/host_vector.h>
+#include "reference_calc.cpp"
+
+#define uchar unsigned char
+
+#define MAX_THREADS 1024
+#define SQRT_MAX_THREADS 32
+
+    #define convert2Dto1D(x, y, numCols) (x) + (y) * (numCols)
+
+__global__
+void mask(char* mask, uchar4* source, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    uchar4 pixel = source[idx];
+    mask[idx] = pixel.x != 255 || pixel.y != 255 || pixel.z != 255;
+  }
+}
+
+__global__
+void findBorderAndInterior(char* border, char* interior, char* mask, size_t numCols, size_t numRows) {
+  unsigned int col = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (col < numCols && row < numRows) {
+    int idx = col + row * numCols;
+    int inMask = mask[idx];
+    int numNeighborsInMask = 0;
+
+    idx = (col - 1) + row * numCols;
+    if (mask[idx]) {
+      numNeighborsInMask++;
+    }
+
+    idx = (col + 1) + row * numCols;
+    if (mask[idx]) {
+      numNeighborsInMask++;
+    }
+
+    idx = col + (row - 1) * numCols;
+    if (mask[idx]) {
+      numNeighborsInMask++;
+    }
+
+    idx = col + (row + 1) * numCols;
+    if (mask[idx]) {
+      numNeighborsInMask++;
+    }
+
+    idx = col + row * numCols;
+    interior[idx] = inMask && (numNeighborsInMask == 4);
+    border[idx] = inMask && (numNeighborsInMask >= 1) && (numNeighborsInMask < 4);
+  }
+}
+
+__global__
+void checkMask(uchar4* destImg, char* mask, uchar4* blendedImg, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    uchar4 newVal;
+
+    if (mask[idx]) {
+      newVal = destImg[idx];
+    }
+    else {
+      newVal.x = newVal.y = newVal.z = 0;
+    }
+
+    blendedImg[idx] = newVal;
+  }
+}
+
+__global__
+void separateChannels(uchar4* combined, uchar* r, uchar* g, uchar* b, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    uchar4 pixel = combined[idx];
+
+    r[idx] = pixel.x;
+    g[idx] = pixel.y;
+    b[idx] = pixel.z;
+  }
+}
+
+__global__
+void recombineChannels(uchar4* combined, uchar* r, uchar* g, uchar* b, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    uchar4 pixel;
+
+    pixel.x = r[idx];
+    pixel.y = g[idx];
+    pixel.z = b[idx];
+
+    combined[idx] = pixel;
+  }
+}
+
+__global__
+void initBuffer(float* dest, uchar* src, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    dest[idx] = (float)src[idx];
+  }
+}
+
+__global__
+void jacobi(float* imgGuessPrev, float* imgGuessNext, char* interior, unsigned char* sourceImg, unsigned char* dstImg, size_t numCols, size_t numRows) {
+  unsigned int col = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int pIdx = col + row * numCols;
+
+
+  if (col < numCols && row < numRows) {
+    if (interior[pIdx]) {
+      float sum1 = 0;
+      float sum2 = 0;
+      
+      int idx = (col - 1) + row * numCols;
+      if (interior[idx]) {
+        sum1 += imgGuessPrev[idx];
+      }
+      else {
+        sum1 += dstImg[idx];
+      }
+      sum2 += sourceImg[pIdx] - sourceImg[idx];
+
+      idx = (col + 1) + row * numCols;
+      if (interior[idx]) {
+        sum1 += imgGuessPrev[idx];
+      }
+      else {
+        sum1 += dstImg[idx];
+      }
+      sum2 += sourceImg[pIdx] - sourceImg[idx];
+
+      idx = col + (row - 1) * numCols;
+      if (interior[idx]) {
+        sum1 += imgGuessPrev[idx];
+      }
+      else {
+        sum1 += dstImg[idx];
+      }
+      sum2 += sourceImg[pIdx] - sourceImg[idx];
+
+      idx = col + (row + 1) * numCols;
+      if (interior[idx]) {
+        sum1 += imgGuessPrev[idx];
+      }
+      else {
+        sum1 += dstImg[idx];
+      }
+      sum2 += sourceImg[pIdx] - sourceImg[idx];
+
+      float newVal = (sum1 + sum2) / 4.0f;
+
+      if (newVal > 255.0f) {
+        newVal = 255.0f;
+      }
+
+      if (newVal < 0.0f) {
+        newVal = 0.0f;
+      }
+
+      imgGuessNext[pIdx] = newVal;
+    }
+    else {
+      imgGuessNext[pIdx] = imgGuessPrev[pIdx];
+    }
+  }
+}
+
+__global__
+void finalizeJacobi(float* imgGuessPrev, uchar* srcImg, uchar* dstImg, char* interior, int numPixels) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < numPixels) {
+    if (interior[idx]) {
+      dstImg[idx] = (uchar)imgGuessPrev[idx];
+    }
+  }
+}
 
 void your_blend(const uchar4* const h_sourceImg,  //IN
                 const size_t numRowsSource, const size_t numColsSource,
                 const uchar4* const h_destImg, //IN
                 uchar4* const h_blendedImg) //OUT
 {
+  // step zero - book keeping
 
+  const int numPixels = numRowsSource * numColsSource;
+  const size_t imageSize = numPixels * sizeof(uchar4);
+
+  uchar4 *d_sourceImg;  // get h_sourceImg, h_destImg, and h_blendedImg on device
+  uchar4 *d_destImg;
+  uchar4 *d_blendedImg;
+
+  checkCudaErrors(cudaMalloc(&d_sourceImg, imageSize));
+  checkCudaErrors(cudaMalloc(&d_destImg, imageSize));
+  checkCudaErrors(cudaMalloc(&d_blendedImg, imageSize));
+
+  checkCudaErrors(cudaMemcpy(d_sourceImg, h_sourceImg, imageSize, cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(d_destImg, h_destImg, imageSize, cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(d_blendedImg, h_destImg, imageSize, cudaMemcpyHostToDevice));
+
+  // step one - compute mask for source image
+  char* d_sourceMask;   // alocate mask on device
+  checkCudaErrors(cudaMalloc(&d_sourceMask, sizeof(char) * numPixels));
+
+  const size_t threads = MAX_THREADS;
+  const size_t blocks = (float)numPixels / MAX_THREADS + 0.5;
+
+  mask<<<blocks, threads>>>(d_sourceMask, d_sourceImg, numPixels);  // fill in mask
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  //checkCudaErrors(cudaMemcpy(d_blendedImg, d_destImg, imageSize, cudaMemcpyDeviceToDevice));  // test passed
+  //checkMask<<<blocks, threads>>>(d_destImg, d_sourceMask, d_blendedImg, numPixels);           // mask computed correctly
+
+  // step two - compute interior and border regions
+  char* d_border;   // alocated arrays for border and interior
+  char* d_interior;
+
+  checkCudaErrors(cudaMalloc(&d_border, sizeof(char) * numPixels));
+  checkCudaErrors(cudaMalloc(&d_interior, sizeof(char) * numPixels));
+
+  dim3 step2Threads(SQRT_MAX_THREADS, SQRT_MAX_THREADS);
+  dim3 step2Blocks(numColsSource / step2Threads.x + 1, numRowsSource / step2Threads.y + 1);
+
+  findBorderAndInterior<<<step2Blocks, step2Threads>>>(d_border, d_interior, d_sourceMask, numColsSource, numRowsSource);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  //checkMask<<<blocks, threads>>>(d_destImg, d_interior, d_blendedImg, numPixels); // after eyeballing it looked correct
+
+  // step three - separate channels
+
+  unsigned char* d_sourceImg_r;
+  unsigned char* d_sourceImg_g;
+  unsigned char* d_sourceImg_b;
+
+  unsigned char* d_dstImg_r;
+  unsigned char* d_dstImg_g;
+  unsigned char* d_dstImg_b;
+
+  size_t channelSize = sizeof(unsigned char) * numPixels;
+
+  checkCudaErrors(cudaMalloc(&d_sourceImg_r, channelSize));
+  checkCudaErrors(cudaMalloc(&d_sourceImg_g, channelSize));
+  checkCudaErrors(cudaMalloc(&d_sourceImg_b, channelSize));
+
+  checkCudaErrors(cudaMalloc(&d_dstImg_r, channelSize));
+  checkCudaErrors(cudaMalloc(&d_dstImg_g, channelSize));
+  checkCudaErrors(cudaMalloc(&d_dstImg_b, channelSize));
+
+  separateChannels<<<blocks, threads>>>(d_sourceImg, d_sourceImg_r, d_sourceImg_g, d_sourceImg_b, numPixels);
+  separateChannels<<<blocks, threads>>>(d_destImg, d_dstImg_r, d_dstImg_g, d_dstImg_b, numPixels);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  // step four - initialize float buffers
+
+  size_t bufferSize = sizeof(float) * numPixels;
+
+  float* d_imageGuessPrev_r;
+  float* d_imageGuessPrev_g;
+  float* d_imageGuessPrev_b;
+
+  checkCudaErrors(cudaMalloc(&d_imageGuessPrev_r, bufferSize));
+  checkCudaErrors(cudaMalloc(&d_imageGuessPrev_g, bufferSize));
+  checkCudaErrors(cudaMalloc(&d_imageGuessPrev_b, bufferSize));
+
+  float* d_imageGuessNext_r;
+  float* d_imageGuessNext_g;
+  float* d_imageGuessNext_b;
+
+  checkCudaErrors(cudaMalloc(&d_imageGuessNext_r, bufferSize));
+  checkCudaErrors(cudaMalloc(&d_imageGuessNext_g, bufferSize));
+  checkCudaErrors(cudaMalloc(&d_imageGuessNext_b, bufferSize));
+
+  initBuffer<<<blocks, threads>>>(d_imageGuessPrev_r, d_sourceImg_r, numPixels);
+  initBuffer<<<blocks, threads>>>(d_imageGuessPrev_g, d_sourceImg_g, numPixels);
+  initBuffer<<<blocks, threads>>>(d_imageGuessPrev_b, d_sourceImg_b, numPixels);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  // step 5 - perform Jacobi iteration 800 times
+
+  const int numJacobiIterations = 800;
+
+  for (int i = 0; i < numJacobiIterations; i++) {
+    jacobi<<<step2Blocks, step2Threads>>>(d_imageGuessPrev_r, d_imageGuessNext_r, d_interior, d_sourceImg_r, d_dstImg_r, numColsSource, numRowsSource);
+    jacobi<<<step2Blocks, step2Threads>>>(d_imageGuessPrev_g, d_imageGuessNext_g, d_interior, d_sourceImg_g, d_dstImg_g, numColsSource, numRowsSource);
+    jacobi<<<step2Blocks, step2Threads>>>(d_imageGuessPrev_b, d_imageGuessNext_b, d_interior, d_sourceImg_b, d_dstImg_b, numColsSource, numRowsSource);
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+    std::swap(d_imageGuessPrev_r, d_imageGuessNext_r);
+    std::swap(d_imageGuessPrev_g, d_imageGuessNext_g);
+    std::swap(d_imageGuessPrev_b, d_imageGuessNext_b);
+  }
+
+  std::swap(d_imageGuessPrev_r, d_imageGuessNext_r);    // results will be in image previous
+  std::swap(d_imageGuessPrev_g, d_imageGuessNext_g);
+  std::swap(d_imageGuessPrev_b, d_imageGuessNext_b);
+
+  // step 6 - create output image
+  finalizeJacobi<<<blocks, threads>>>(d_imageGuessPrev_r, d_sourceImg_r, d_dstImg_r, d_interior, numPixels);
+  finalizeJacobi<<<blocks, threads>>>(d_imageGuessPrev_g, d_sourceImg_g, d_dstImg_g, d_interior, numPixels);
+  finalizeJacobi<<<blocks, threads>>>(d_imageGuessPrev_b, d_sourceImg_b, d_dstImg_b, d_interior, numPixels);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  // book keeping
+
+  recombineChannels<<<blocks, threads>>>(d_blendedImg, d_dstImg_r, d_dstImg_g, d_dstImg_b, numPixels);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+
+  checkCudaErrors(cudaMemcpy(h_blendedImg, d_blendedImg, imageSize, cudaMemcpyDeviceToHost));  // copy results to host
+  
   /* To Recap here are the steps you need to implement
   
      1) Compute a mask of the pixels from the source image to be copied
@@ -110,4 +422,18 @@ void your_blend(const uchar4* const h_sourceImg,  //IN
 
       to catch any errors that happened while executing the kernel.
   */
+
+
+
+  /* The reference calculation is provided below, feel free to use it
+     for debugging purposes. 
+   */
+
+  /*
+    uchar4* h_reference = new uchar4[srcSize];
+    reference_calc(h_sourceImg, numRowsSource, numColsSource,
+                   h_destImg, h_reference);
+
+    checkResultsEps((unsigned char *)h_reference, (unsigned char *)h_blendedImg, 4 * srcSize, 2, .01);
+    delete[] h_reference; */
 }
